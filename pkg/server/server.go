8257ae054992a2f597588ce6d603f762956659e3
cricketue <cricketue@gmail.com>
2017-07-06 15:33:27 +0800

    gfish

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -1,7 +1,26 @@
 package server
 
+import (
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"net/http"
+)
 
 type Server struct {
-   
+	mux  *http.server
+	grpc *grpc.Server
+}
 
-} 
+func NewServer() (*server,error){
+  if _,err := net.ResolveTCPAddr("tcp","127.0.0.1") ;err != {
+     return nil,errors.Errorf("unable to resolve RPC address %q: %v", "127.0.0.1", err)
+  }
+  
+  s := &Server{
+     mux:  http.NewServeMux(),
+  }
+
+  g.
+
+
+}
