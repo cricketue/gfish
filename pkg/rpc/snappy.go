a13b69fee0b1d4ff5b08bc29cb9bb7b0802bbbab
cricketue <cricketue@gmail.com>
2017-07-06 19:20:29 +0800

    gfish

--- pkg/rpc/snappy.go
+++ pkg/rpc/snappy.go
@@ -1,21 +1,3 @@
-package rpc
 
-import (
-	"github.com/golang/snappy"
-	"io"
-	"io/util"
-	"sync"
-)
-
-var snappyWriterPool sync.pool
-var snappyReaderPool sync.pool
-
-type snappyCompressor struct {
-}
-
-
-func (snappyCompressor) Do(w io.Writer,p []btye) error{
-  z,ok := io.
-}
 
 
